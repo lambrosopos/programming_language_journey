b5cc225f5ef301369affdb0aa3725833c66d1cde
lambrosopos <lambrosopos@gmail.com>
2021-05-22 23:57:24 +0900

    update better calculator example

---
+++ c_basics/better_calculator.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num1, num2;
+  char op;
+
+  cout << "Enter first number: ";
+  cin >> num1;
+
+  cout << "Enter operator: ";
+  cin >> op;
+
+  cout << "Enter second number: ";
+  cin >> num2;
+
+  int result;
+
+  if (op == '+') {
+    result = num1 + num2;
+  } else if (op == '-') {
+    result = num1 - num2;
+  } else if (op == '*') {
+    result = num1 * num2;
+  } else if (op == '/') {
+    result = num1 / num2;
+  } else {
+    cout << "Operator invalid";
+  }
+
+
+  cout << "Result: " << result;
+
+  return 0;
+}
