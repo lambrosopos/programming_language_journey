7ac2aacf811c5e45df5cdfcb393915cbad972ba4
lambrosopos <lambrosopos@gmail.com>
2021-05-24 22:44:41 +0900

    add comments

---
+++ c_basics/comments.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // single line comments
+  cout << "Comments are fun"; // you can even write code here
+
+  /*
+   * This is a multiline comment
+   * you can write multiple lines
+   */
+
+  return 0;
+}
