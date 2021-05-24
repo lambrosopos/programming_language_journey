9d4740793bc22c83ce86ace32d434efbb9febc87
lambrosopos <lambrosopos@gmail.com>
2021-05-24 22:35:59 +0900

    basic nested arrays

---
+++ c_basics/nested_arrays.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int numberGrid[3][2] = {
+    {1, 2},
+    {3, 4},
+    {5, 6},
+  };
+
+  cout << "numberGrid[0][1]: " << numberGrid[0][1] << endl;
+
+  return 0;
+}
