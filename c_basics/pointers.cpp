1a559120c60e264ffadbc829104705d5fc78b3f7
lambrosopos <lambrosopos@gmail.com>
2021-05-25 22:01:24 +0900

    adding pointer variables and dereferencing example

--- c_basics/pointers.cpp
+++ c_basics/pointers.cpp
@@ -7,8 +7,15 @@ int main() {
   double gpa = 2.7;
   string name = "Spongebob";
 
+  // creating a pointer variable
+  // pointer variables are variables that store the memory address
+  int *pAge = &age;
+
   // printing out the memory address of the variable
-  cout << &age << endl;
+  cout << pAge << endl;
+
+  // printing out the actual value of the memory address
+  cout << *pAge << endl;
 
   return 0;
 }
