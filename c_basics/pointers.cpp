245b75d2dbe35047933f9a1115103d3125d22b83
lambrosopos <lambrosopos@gmail.com>
2021-05-25 21:58:34 +0900

    basic pointers and memory addresses

---
+++ c_basics/pointers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int age = 19;
+  double gpa = 2.7;
+  string name = "Spongebob";
+
+  // printing out the memory address of the variable
+  cout << &age << endl;
+
+  return 0;
+}
