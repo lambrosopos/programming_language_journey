7b9971ef69d35b0496200e91bbf25e79fb01c6b1
lambrosopos <lambrosopos@gmail.com>
2021-05-26 22:26:15 +0900

    extra pointers and dereferencing examples

--- c_basics/pointers.cpp
+++ c_basics/pointers.cpp
@@ -1,11 +1,16 @@
+#include <string>
 #include <iostream>
 
 using namespace std;
 
 int main() {
+  // you have to store the same value of pointer is pointing at
+  string name = "Spongebob";
+  string *pName = &name;
+
   int age = 19;
   double gpa = 2.7;
-  string name = "Spongebob";
+
 
   // creating a pointer variable
   // pointer variables are variables that store the memory address
@@ -14,8 +19,13 @@ int main() {
   // printing out the memory address of the variable
   cout << pAge << endl;
 
+  // dereferencing
   // printing out the actual value of the memory address
   cout << *pAge << endl;
+
+  // you can also just dereference a value
+  // this is the same as *pAge
+  cout << *&age << endl;
 
   return 0;
 }
