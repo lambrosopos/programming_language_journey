d4460484dc79c28df520c51ce89b91973592c0a4
lambrosopos <lambrosopos@gmail.com>
2021-05-26 22:36:45 +0900

    adding book class

--- c_basics/basic_classes.cpp
+++ c_basics/basic_classes.cpp
@@ -2,10 +2,31 @@
 
 using namespace std;
 
+/* Classes
+ * classes are basically a new data type
+ * we can make custom types to represent data
+ */
+
+// class names are capitals
+class Book {
+  // mapping out a blueprint for a book datatype
+  public:
+    string title;
+    string author;
+    int pages;
+};
+
+
 int main() {
-  string name = "Spongebob";
-  double pi = 3.14;
-  char favoriteLetter = 'G';
+  // creating an object which is an instance of the class
+  Book book1;
+  book1.title = "Alice in Wonderland";
+  book1.author = "Lewis Carroll";
+  book1.pages = 100;
+
+  cout << book1.pages << endl;
+  cout << book1.title << endl;
+  cout << book1.author << endl;
 
   return 0;
 }
