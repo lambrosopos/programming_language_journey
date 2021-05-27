f3273c6427c4f13f4dddf01a329467d1949cb5a3
lambrosopos <lambrosopos@gmail.com>
2021-05-27 23:21:52 +0900

    adding class methods

--- c_basics/basic_classes.cpp
+++ c_basics/basic_classes.cpp
@@ -29,24 +29,35 @@ class Book {
 
       cout << "Creating Object: " << title << endl;
     }
+
+    // methods for class
+    bool isOver100Pages() {
+      if (pages > 100) {
+        return true;
+      }
+      return false;
+    }
 };
 
 
 int main() {
   // creating an object which is an instance of the class
-  Book book1("Alice in Wonderland", "Lewis Carroll", 100);
+  Book book1("Alice in Wonderland", "Lewis Carroll", 500);
   //book1.title = "Alice in Wonderland";
   //book1.author = "Lewis Carroll";
   //book1.pages = 100;
-  Book book2;
+  Book book2("Spongebob", "Patrick", 50);
 
   cout << book1.pages << endl;
   cout << book1.title << endl;
   cout << book1.author << endl;
 
+  cout << book1.isOver100Pages() << endl;
+
   cout << book2.pages << endl;
   cout << book2.title << endl;
   cout << book2.author << endl;
 
+  cout << book2.isOver100Pages() << endl;
   return 0;
 }
