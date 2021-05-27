5ef11d0b74c5811be070d32d9fedcdd59219ae60
lambrosopos <lambrosopos@gmail.com>
2021-05-27 23:33:34 +0900

    adding setter method with conditions

--- c_basics/more_classes.cpp
+++ c_basics/more_classes.cpp
@@ -12,12 +12,20 @@ class Movie {
     Movie(string aTitle, string aDirector, string aRating) {
       title = aTitle;
       director = aDirector;
-      rating = aRating;
+      setRating(aRating);
+    }
+
+    void setRating(string aRating) {
+      if (aRating == "G" || aRating == "PG" || aRating == "R" || aRating == "PG-13") {
+        rating = aRating;
+      };
     }
 };
 
 int main() {
   Movie avengers("The Avengers", "joss Whedon", "PG-13");
+
+  avengers.setRating("R");
 
   cout << avengers.rating << endl;
   return 0;
