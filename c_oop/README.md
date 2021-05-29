df417d3e7a905c14dbf9e5a6ee6f505cf298ff57
lambrosopos <lambrosopos@gmail.com>
2021-05-29 23:41:10 +0900

    starting c++ oop journey

---
+++ c_oop/README.md
@@ -0,0 +1,1 @@
+This c++ oop is based on the tutorial from freecodecamp video [here](https://youtu.be/wN0x9eZLix4) is the link
---
+++ c_oop/starting_oop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/* OOP
+ * Able to represent real-life objects,
+ * explain such objects to a computer
+ */
+
+int main()
+{
+
+  return 0;
+}
