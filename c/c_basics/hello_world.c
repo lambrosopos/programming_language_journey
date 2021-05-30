00b5286e17eedb947bdda2cbdbd6383c122eee7e
lambrosopos <lambrosopos@gmail.com>
2021-05-31 00:45:39 +0900

    basic printing in c

--- c/c_basics/hello_world.c
+++ c/c_basics/hello_world.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* You build a c file
+ * compile it
+ * and tell the computer to run it
+ *
+ * on mac you can use 'gcc -o output_file input_file'
+ */
 int main()
 {
     printf("Hello world!\n");
     return 0;
 }
+
+
---
+++ c/c_basics/variables_in_c.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Storing multiple characters in c -> making an array of char
+    char characterName[] = "Spongebob";
+
+    // Storing numbers -> use integers
+    int characterAge = 14;
+
+    printf("There once was a man named %s\n", characterName);
+    printf("he was %d years old\n", characterAge);
+    printf("He really like balloons\n");
+    printf("but liked to play with Patrick.\n");
+
+    return 0;
+}
