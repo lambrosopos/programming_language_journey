fc1ea5c6cc62af9489475ffd77acb168e6241f64
lambrosopos <lambrosopos@gmail.com>
2021-05-31 21:52:23 +0900

    datatypes in c

---
+++ c/c_basics/datatypes_in_c.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* 3 main data types in c
+ *
+ * integer, double, character
+ * 
+ * There is additional 'datatype' called string
+ * which is simply a collection of datatypes connected together
+ */
+
+int main()
+{
+    int age = 40;
+
+    double gpa = 3.6;
+
+    // Storing a single character using ''
+    char grade = 'A';
+
+    return 0;
+}
