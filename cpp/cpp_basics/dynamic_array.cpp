5bdc572a67fbdf3e30897b201ba37656656ce31a
lambrosopos <lambrosopos@gmail.com>
2021-06-07 22:55:46 +0900

    add dynamic array - vector

---
+++ cpp/cpp_basics/dynamic_array.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+/* dynamic array - vector
+ * in the std namespace there is vector
+ * which is essentially a dynamic array
+ *
+ * vector is basically a set that allows duplicate elements
+ * which is basically an array.
+ *
+ * however unlike an array, a vector can resize.
+ *
+ * Thus you don't need to give it a specific size.
+ *
+ * You just create a vector and add elements to it.
+ *
+ * When a vector is initiated, it creates space for certain amount of elements
+ * and whenever more space is needed, it creates a new space and transfers the elements.
+ *
+ *
+ */
+
+int main()
+{
+    // with arrays
+    // you can only add 5 elements to this array
+    int *vertices = new int[5];
+
+    // with vector
+    vector <int> new_vertices; 
+
+    cin.get(); 
+}
+
