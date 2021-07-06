da4b60c959cfdf1b123333e285a1974b484f362a
lambrosopos <lambrosopos@gmail.com>
2021-07-06 21:29:29 +0900

    new_id simple if solution

--- python/coding_problems/programmers/new_id.py
+++ python/coding_problems/programmers/new_id.py
@@ -8,18 +8,25 @@ def main(new_id):
     for idx, char in enumerate(new_id):
         if re.match(r'[a-zA-Z0-9._-]', char):
             if char == '.':
-                if (idx + 1) <= len(new_id) and new_id[idx + 1] == '.': 
+                try:
+                    if new_id[idx + 1] == '.': 
+                        continue
+                except IndexError:
                     continue
             answer += char
 
     if answer[0] == '.':
         answer = answer[1:]
 
+
     if answer == '':
         answer += 'a'
 
     if len(answer) >= 16:
         answer = answer[:15]
+
+    if len(answer) != 0 and answer[-1] == '.':
+        answer = answer[:-1]
 
     while len(answer) <= 2:
         answer += answer[-1]
@@ -28,5 +35,14 @@ def main(new_id):
 
 
 if __name__ == "__main__":
-    answer = main("...!@BaT#*..y.abcdefghijklm")
-    print(answer)
+    test_strings = [
+        "...!@BaT#*..y.abcdefghijklm",
+        "z-+.^.",
+        "=.=",
+        "123_.def",
+        "abcdefghijklmn.p"
+    ]
+
+    for s in test_strings:
+        answer = main(s)
+        print(answer)
