cc2d61f60aa97f92a7d3e31241566eb946a4e745
qetroux <lambrosopos@gmail.com>
2021-07-15 22:24:23 +0900

    solved math problem

--- python/coding_problems/programmers/give_up_math.py
+++ python/coding_problems/programmers/give_up_math.py
@@ -1,29 +1,41 @@
+import math
+
 def solution(answers):
-    # 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
-    # 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
-    # 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
     pattern_1 = [1, 2, 3, 4, 5] #=> modulo 5
     pattern_2 = [1, 3, 4, 5] #=> if odd = 2 if even modulo 4
     pattern_3 = [3, 1, 2, 4, 5] #=> divide by 2 and round up
 
-    person_1, person_2, person_3 = (0, 0, 0)
+    persons = [0, 0, 0]
 
-    for val, idx in enumerate(answers):
-        if val == pattern_1[idx % 4]:
-            person_1 += 1
+    for idx, val in enumerate(answers):
+        if val == pattern_1[idx % 5]:
+            persons[0] += 1
 
         if idx % 2 == 0 and val == 2:
-            person_2 += 1
-        elif idx % 2 == 1 and val == pattern_2[idx % 3]:
-            person_2 += 1
+            persons[1] += 1
+        elif idx % 2 == 1 and val == pattern_2[idx % 4]:
+            persons[1] += 1
 
-        if val == pattern_3[round(idx / 2)]:
-            pattern_3 += 1
+        if val == pattern_3[math.floor(idx / 2)]:
+            persons[2] += 1
 
-    return (person_1, person_2, person_3)
+    if persons[0] == persons[1] == persons[2]:
+        return [1, 2, 3]
+    
+    highest_answer = persons[0]
+    highest_person = []
+
+    for idx, p in enumerate(persons):
+        if p > highest_answer:
+            highest_person = [idx + 1]
+            highest_answer = p
+        elif p == highest_answer:
+            highest_person.append(idx + 1)
+
+    return highest_person
 
 
 if __name__ == "__main__":
-    problems = [1, 2, 3, 4, 5]
+    problems = [1, 2, 1, 2, 2]
     ans = solution(problems)
     print(ans)
