56f929c8ee50a5160d4ac7b7c5c7d0225b3447fe
lambrosopos <lambrosopos@gmail.com>
2024-03-29 22:36:24 +0900

    feat: broadcast example -> udp using 255 for address

---
+++ network_programming/tcp_ip_programming_book/chapter_14/news_sender_brd.c
@@ -0,0 +1,52 @@
+#include <asm-generic/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 30
+
+void error_handling(char *message);
+
+int main(int argc, char *argv[]) {
+  int send_sock;
+  struct sockaddr_in broad_adr;
+  FILE *fp;
+  char buf[BUF_SIZE];
+  int so_brd=1; // socket broadcast option
+
+  if(argc != 3) {
+    printf("Usage : %s <Broadcast IP> <PORT>\n", argv[0]);
+  }
+
+  send_sock = socket(PF_INET, SOCK_DGRAM, 0);
+  memset(&broad_adr, 0, sizeof(broad_adr));
+  broad_adr.sin_family = AF_INET;
+  broad_adr.sin_addr.s_addr = inet_addr(argv[1]);
+  broad_adr.sin_port = htons(atoi(argv[2]));
+
+  setsockopt(send_sock, SOL_SOCKET, SO_BROADCAST, (void *)&so_brd, sizeof(so_brd));
+
+  if((fp = fopen("news.txt", "r")) == NULL) {
+    error_handling("fopen() error");
+  }
+
+  while(!feof(fp)) {
+    fgets(buf, BUF_SIZE, fp);
+    sendto(send_sock, buf, BUF_SIZE, 0, (struct sockaddr*)&broad_adr, sizeof(broad_adr));
+    sleep(2);
+  }
+
+  fclose(fp);
+  close(send_sock);
+  return 0;
+}
+
+void error_handling(char* message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
